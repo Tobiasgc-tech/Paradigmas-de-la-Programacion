e506bc03143c27f1331d4adb8ab4a0d890aa3eef
Tobiasgc-tech <tobiasgabrielcogliano@gmail.com>
2025-03-28 15:30:46 -0300

    Ejercio2

--- Practicas/01.hs
+++ Practicas/01.hs
@@ -15,4 +15,32 @@ predecesor = subtract 1
 :t predecesor: Float -> Float
 
 evaluarEnCero = \f -> f 0
+evaluarEnCero = (Float -> Float) -> Float
+
+dosVeces = \f -> f . f
+:t dosVeces = (a -> a) -> a -> a
+
+flipAll = map flip
+:t flipAll = [(a -> b -> c)] -> [(b -> a -> c)]
+
+flipRaro = flip flip
+t: flipRaro = ((a -> b -> c) -> (b -> a -> c)) -> ((b -> a -> c) -> (a -> b -> c))
 -}
+
+--Ejercicio2
+
+--Ejercicio3
+sumFoldr:: [Int] -> Int
+sumFoldr xs = foldr (+) 0 xs
+
+elemFoldr:: Int -> [Int] -> Bool
+elemFoldr elemento xs = foldr (\x res -> x == elemento || res) False xs
+
+concatFoldr:: [a] -> [a] -> [a]
+concatFoldr xs ys = foldr (:) ys xs
+
+filterFoldr:: (a -> Bool) -> [a] -> [a]
+filterFoldr f xs = foldr (\x r -> if f x then x:r else r) [] xs
+
+mapFoldr:: (a -> b) -> [a] -> [b]
+mapFoldr f xs = foldr (\x r -> f x :r) [] xs
